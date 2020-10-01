9463a450ebb42b8d6dcbeeca5734c8144ceb0e06
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-01 17:17:43 -0400

    added standard colors

--- app/components/Panels.tsx
+++ app/components/Panels.tsx
@@ -2,9 +2,11 @@ import * as React from 'react';
 
 import { Classes, ITreeNode, Tree } from '@blueprintjs/core';
 
+import styles from './panels.scss';
+
 export const Panel = ({ children }: any) => {
   return (
-    <div className="panel">
+    <div className={styles.panel}>
       <div className="content">PANEL</div>
     </div>
   );
