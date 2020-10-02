f19e79dc16c409d7d073e7edc9f088fb4ec6ec55
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-02 17:07:43 -0400

    having trouble styling the blueprint table

--- app/features/project/project.tsx
+++ app/features/project/project.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Column, Table } from '@blueprintjs/table';
+import { Cell, Column, Table } from '@blueprintjs/table';
 import { PROJECTS_RECENT } from '../../data/data.home.initial';
+import styles from './project.scss';
 
 export default function ProjectSelection() {
   // const dispatch = useDispatch();
   // const value = useSelector();
 
-  const recentProjects = PROJECTS_RECENT.list;
+  const projs = PROJECTS_RECENT.list;
+  const rendNames = projs.map((i) => <Cell key={`${i.id}`}>{i.name}</Cell>);
+  const cellRenderer = (rowIndex: number) => {
+    return <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>;
+  };
 
   return (
-    <div className="project">
+    <div className={styles.project}>
       <div className="project__display">
         <h4>Recent Projects</h4>
         <div className="display__options">
-          <Table numRows={recentProjects.length}>
-            <Column name="Recent Projects" />
+          <Table
+            numRows={projs.length}
+            // className={styles.project__display__table}
+          >
+            <Column cellRenderer={cellRenderer} />
           </Table>
         </div>
       </div>
