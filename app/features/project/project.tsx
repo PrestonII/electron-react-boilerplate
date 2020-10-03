8b20d5a907848ca0dd4b0a7a281e69eff7a991d2
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-03 08:19:06 -0400

    created project display items

--- app/features/project/project.tsx
+++ app/features/project/project.tsx
@@ -31,3 +31,30 @@ export default function ProjectSelection() {
     </div>
   );
 }
+
+type ProjectItemProps = {
+  name: string;
+  date: string;
+};
+
+const RecentProjectItem = (props: ProjectItemProps) => {
+  const { name, date } = props;
+
+  return <DisplayItem key={name} value={date} />;
+}
+
+const DisplayItem = (props: DisplayItemProps) => {
+  const { key, value } = props;
+
+  return (
+    <div className="displayItem">
+      <p className="displayItem__key">{key}</p>
+      <p className="displayItem__value">{value}</p>
+    </div>
+  );
+};
+
+type DisplayItemProps = {
+  key: string;
+  value: string;
+}
