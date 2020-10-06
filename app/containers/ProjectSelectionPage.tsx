2eb7d86fc1ee6c80c19a8781e96a82317cd924dc
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-06 10:44:01 -0400

    added buttons

--- app/containers/ProjectSelectionPage.tsx
+++ app/containers/ProjectSelectionPage.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 import ProjectSelector from '../features/projectSelector/projectSelector';
 import ProjectExample from '../features/projectSelector/projectSelector.example';
 import { PageWithPanel } from '../components/Pages';
 
 export default function ProjectSelectionPage() {
   const panelData = {
-    title: 'TestTile',
-    content: ['sfasfsf', 'fsdg'],
-  }
+    // title: 'TestTile',
+    content: [
+      <Button
+        key="new"
+        onClick={() => {
+          alert('clicked');
+        }}
+      >
+        <h1>Create New</h1>
+      </Button>,
+      <Button
+        key="open"
+        onClick={() => {
+        alert('clicked');
+        }}
+      >
+        <h1>Open Project</h1>
+      </Button>,
+    ],
+  };
 
   return (
     <PageWithPanel panelContent={panelData}>
