c0c342702e83578ef626154583369aeea63b4b60
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-06 08:55:47 -0400

    panels are sort of working?

--- app/components/Panels.tsx
+++ app/components/Panels.tsx
@@ -3,22 +3,30 @@ import styles from './panels.scss';
 
 export interface IPanelContent {
   title: string;
-  content: Array<any>;
+  content: [];
 }
 
 export class Panel extends React.Component<IPanelContent> {
   static defaultProps = {
     title: 'Program Dashboard',
-  }
+  };
 
   render() {
     const { title, content } = this.props;
 
+    const mapped = content.map((x, index) => {
+      // console.log(index);
+      const key = index;
+      const value = `${x} - ${key}`;
+      // console.log(value);
+      return <span key={key}>{value}</span>;
+    });
+
     return (
       <div className={styles.panel}>
         <div className="content">{title}</div>
-        { content }
+        {content}
       </div>
-    )
+    );
   }
 }
--- app/containers/ProjectSelectionPage.tsx
+++ app/containers/ProjectSelectionPage.tsx
@@ -4,8 +4,13 @@ import ProjectExample from '../features/projectSelector/projectSelector.example'
 import { PageWithPanel } from '../components/Pages';
 
 export default function ProjectSelectionPage() {
+  const panelData = {
+    title: 'TestTile',
+    content: ['sfasfsf', 'fsdg'],
+  }
+
   return (
-    <PageWithPanel >
+    <PageWithPanel panelContent={panelData}>
       <ProjectSelector />
       {/* <ProjectExample /> */}
     </PageWithPanel>
