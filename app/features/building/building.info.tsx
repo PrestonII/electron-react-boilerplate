3fc713532d53e2051d775122dfe1c0b3bb276bf6
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-10-07 16:16:34 -0400

    styling done

--- app/features/building/building.info.tsx
+++ app/features/building/building.info.tsx
@@ -5,7 +5,14 @@ import ToggleButton from '../../components/ToggableButton';
 import QnA from '../../components/Question';
 
 function BuildingInformation() {
+
   const Q1 = (
+    <p>
+      What is your client's name?
+    </p>
+  );
+
+  const Q2 = (
     <p>
       Which
       <em> units </em>
@@ -13,81 +20,61 @@ function BuildingInformation() {
     </p>
   );
 
+  const Q3 = (
+    <p>
+      Is your space a
+      <em> multi-tenant </em>
+      or
+      <em> single-tenant? </em>
+    </p>
+  );
+
+  const Q4 = (
+    <p>
+      Is this space a
+      <em> broadcast </em>
+      studio?
+    </p>
+  );
+
   return (
-    <>
-      <div className={styles.page}>
-        <div className={styles.page__title}>
-          <h2>General Space Information</h2>
-        </div>
-
-        <div className={styles.page__content__fill}>
-          <div className={styles.page__content__question}>
-            <p>What's your client's name</p>
-            <div className={styles.page__content__question__textbox}>
-              <TextField
-                className={styles.page__content__question__textbox__standard}
-                id="standard-basic"
-                label="Client Name"
-              />
-            </div>
-          </div>
-
-          <QnA
-            question={Q1}
-            answers={[
-              <ToggleButton content="Imperial" />,
-              <ToggleButton content="Metric" />,
-            ]}
-          />
-          {/* <div className={styles.page__content__question}>
-            <p>
-              Which
-              <em> units </em>
-              should we use to measure your space?
-            </p>
-            <div className={styles.page__content__question__answers}>
-              <ToggleButton content='Imperial' />
-              <ToggleButton content='Metric' />
-            </div>
-          </div> */}
-
-          <div className="page__content__question">
-            <p>
-              Is your space a
-              <em> multi-tenant </em>
-              or
-              <em> single-tenant? </em>
-            </p>
-            <div className={styles.page__content__question__answers}>
-              <Button variant="outlined">Single</Button>
-              <Button variant="outlined">Multi</Button>
-            </div>
-          </div>
-
-          <div className="page__content__question">
-            <p>
-              Is this space a
-<em> broadcast </em>
-              studio?
-            </p>
-            <div className={styles.page__content__question__answers}>
-              <Button variant="outlined">Yes</Button>
-              <Button variant="outlined">No</Button>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.page__navigation}>
-          <div className={styles.page__naviation__left}>
-            <Button variant="outlined">Back</Button>
-          </div>
-
-          <div className={styles.page__naviation__right}>
-            <Button variant="outlined">Next</Button>
-          </div>
-        </div>
+    <div className={styles.page}>
+      <div className={styles.page__title}>
+        <h2>General Space Information</h2>
       </div>
-    </>
+
+      <QnA
+        question={Q1}
+        answers={[
+          <ToggleButton content="Imperial" />,
+          <ToggleButton content="Metric" />,
+        ]}
+      />
+
+      <QnA
+        question={Q2}
+        answers={[
+          <ToggleButton content="Imperial" />,
+          <ToggleButton content="Metric" />,
+        ]}
+      />
+
+      <QnA
+        question={Q3}
+        answers={[
+          <ToggleButton content="Single" />,
+          <ToggleButton content="Multi" />,
+        ]}
+      />
+
+      <QnA
+        question={Q4}
+        answers={[
+          <ToggleButton content="Yes" />,
+          <ToggleButton content="No" />,
+        ]}
+      />
+    </div>
   );
 }
 
