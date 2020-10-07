59115a3d07a108063477ec6ee790584cdc22ac77
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-07 10:40:18 -0400

    starting on the building page

--- app/features/building/building.info.tsx
+++ app/features/building/building.info.tsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import { Button, TextField } from '@material-ui/core';
+import styles from './styles.building.info.scss';
 
 function BuildingInformation() {
-  return <h2>General Building</h2>;
+  return (
+    <>
+      <div className={styles.page}>
+        <div className={styles.page__title}>
+          <h2>General Building Information</h2>
+        </div>
+        <div className="page__content">
+          <div className="page__content__question1">
+            <p>What's your client's name</p>
+            <TextField id="standard-basic" label="Client Name" />
+          </div>
+          <div className="page__content__question2">
+            <p>
+              Which
+              <em> units </em>
+              should we use to measure your space?
+            </p>
+            <div className="page__content__question2__answers">
+              <Button variant="outlined" color="secondary">
+                Imperial
+              </Button>
+              <Button variant="outlined" color="secondary">
+                Metric
+              </Button>
+            </div>
+          </div>
+          <div className="page__content__question3" />
+          <div className="page__content__question4" />
+          <div className="page__content__question5" />
+        </div>
+        <div className="page__navigation">
+          <Button variant="outlined" color="secondary">
+            Back
+          </Button>
+          <Button variant="outlined" color="secondary">
+            Next
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default BuildingInformation;
