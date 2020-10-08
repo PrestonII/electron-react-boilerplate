ed368f83e5187f9a52d822af4ab34acf57e9842f
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-08 14:10:13 -0400

    optimized imports

--- app/components/NavigationalButtons.tsx
+++ app/components/NavigationalButtons.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 
 function InternalNavigationalButton({
