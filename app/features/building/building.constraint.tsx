b2bdf12adae3d47f06cc459e386e788823708f6d
KP-Kim <57761471+KP-Kim@users.noreply.github.com>
2020-10-08 14:43:15 -0400

    question texts in

--- app/features/building/building.constraint.tsx
+++ app/features/building/building.constraint.tsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { TextField } from '@material-ui/core';
+import styles from './styles.building.constraint.scss';
 
 function BuildingConstraint () {
-  return "hello this has been linked"
+
+  const Q1 = (
+    <p>
+      What's the
+      <em> total RSF of the space? </em>
+    </p>
+  );
+
+  const Q2 = (
+    <p>
+      What's the
+      <em> RSF loss factor? </em>
+    </p>
+  );
+
+  const Q3 = (
+    <p>
+      Is your space a
+      <em> multi-tenant </em>
+      or
+      <em> single-tenant? </em>
+    </p>
+  );
+
+  return (
+    <div className={styles.whats}><p> test </p></div>
+  )
 }
 
 export default BuildingConstraint;
